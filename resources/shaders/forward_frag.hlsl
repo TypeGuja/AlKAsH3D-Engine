781cad6fdec4b4c98ebe43130250a7d76889a63b
Type_Guja <gusamogus84@yandex.ru>
2026-03-01 19:20:59 +0400

    shaders

--- resources/shaders/forward_frag.hlsl
+++ resources/shaders/forward_frag.hlsl
@@ -1,43 +1,12 @@
-struct PSInput
+cbuffer FrameCB : register(b0)
 {
-    float4 position : SV_POSITION;
-    float3 normal : NORMAL;
-    float2 texcoord : TEXCOORD;
-    float3 worldPos : WORLDPOS;
+    float4x4 uView;
+    float4x4 uProj;
+    float4x4 uModel;
+    float4   uTint;
 };
 
-cbuffer PerFrame : register(b0)
+float4 main() : SV_TARGET
 {
-    float4x4 view;
-    float4x4 projection;
-    float4x4 viewProjection;
-    float4 cameraPos;
-    float4 lightDir;
-    float4 lightColor;
-    float4 ambientColor;
-};
-
-cbuffer PerObject : register(b1)
-{
-    float4x4 model;
-    float4x4 modelInverseTranspose;
-};
-
-Texture2D albedoTexture : register(t0);
-SamplerState albedoSampler : register(s0);
-
-float4 main(PSInput input) : SV_Target
-{
-    // Базовый оранжевый цвет
-    float4 baseColor = float4(1.0, 0.5, 0.2, 1.0);
-    
-    // Простое освещение
-    float3 normal = normalize(input.normal);
-    float3 lightDirNorm = normalize(-lightDir.xyz);
-    float diff = max(dot(normal, lightDirNorm), 0.0);
-    
-    float4 result = baseColor * (ambientColor + diff * lightColor);
-    result.a = 1.0;
-    
-    return result;
+    return uTint;
 }
--- resources/shaders/forward_vert.hlsl
+++ resources/shaders/forward_vert.hlsl
@@ -1,45 +1,26 @@
-struct VSInput
+cbuffer FrameCB : register(b0)
 {
-    float3 position : POSITION;
-    float3 normal : NORMAL;
-    float2 texcoord : TEXCOORD;
+    float4x4 uView;
+    float4x4 uProj;
+    float4x4 uModel;
+    float4   uTint;
 };
 
-struct VSOutput
+struct VS_IN
 {
-    float4 position : SV_POSITION;
-    float3 normal : NORMAL;
-    float2 texcoord : TEXCOORD;
-    float3 worldPos : WORLDPOS;
+    float3 pos : POSITION;
 };
 
-cbuffer PerFrame : register(b0)
+struct VS_OUT
 {
-    float4x4 view;
-    float4x4 projection;
-    float4x4 viewProjection;
-    float4 cameraPos;
-    float4 lightDir;
-    float4 lightColor;
-    float4 ambientColor;
+    float4 pos : SV_POSITION;
 };
 
-cbuffer PerObject : register(b1)
+VS_OUT main(VS_IN i)
 {
-    float4x4 model;
-    float4x4 modelInverseTranspose;
-};
-
-VSOutput main(VSInput input)
-{
-    VSOutput output;
-    
-    float4 worldPos = mul(float4(input.position, 1.0), model);
-    output.position = mul(worldPos, viewProjection);
-    output.worldPos = worldPos.xyz;
-    
-    output.normal = mul(input.normal, (float3x3)modelInverseTranspose);
-    output.texcoord = input.texcoord;
-    
-    return output;
+    VS_OUT o;
+    float4 world = mul(uModel, float4(i.pos, 1.0));
+    float4 view  = mul(uView,  world);
+    o.pos = mul(uProj, view);
+    return o;
 }
