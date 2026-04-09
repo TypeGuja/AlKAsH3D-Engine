74ffa2182537f14e429dbea3193877e0b8ddd290
Type_Guja <gusamogus84@yandex.ru>
2026-04-09 22:25:26 +0400

    rust

---
+++ alkash3d-rust/target/release/build/proc-macro2-0a751879c6dff614/build_script_build-0a751879c6dff614.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\release\build\proc-macro2-0a751879c6dff614\build_script_build-0a751879c6dff614.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.106\build.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\release\build\proc-macro2-0a751879c6dff614\build_script_build-0a751879c6dff614.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.106\build.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.106\build.rs:
---
+++ alkash3d-rust/target/release/build/quote-b6f4f2c0f9f25cd8/build_script_build-b6f4f2c0f9f25cd8.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\release\build\quote-b6f4f2c0f9f25cd8\build_script_build-b6f4f2c0f9f25cd8.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.45\build.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\release\build\quote-b6f4f2c0f9f25cd8\build_script_build-b6f4f2c0f9f25cd8.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.45\build.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quote-1.0.45\build.rs:
