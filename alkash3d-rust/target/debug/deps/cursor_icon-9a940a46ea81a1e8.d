8e6d5a290d1878b6f18671663342eb0e981a9f7e
Type_Guja <gusamogus84@yandex.ru>
2026-04-26 05:28:16 +0400

    rust

---
+++ alkash3d-rust/target/debug/deps/cursor_icon-9a940a46ea81a1e8.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\debug\deps\cursor_icon-9a940a46ea81a1e8.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cursor-icon-1.2.0\src\lib.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\debug\deps\libcursor_icon-9a940a46ea81a1e8.rmeta: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cursor-icon-1.2.0\src\lib.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cursor-icon-1.2.0\src\lib.rs:
