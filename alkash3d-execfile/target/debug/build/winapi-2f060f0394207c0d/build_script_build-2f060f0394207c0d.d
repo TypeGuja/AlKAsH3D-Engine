60801bb064adfbfcf03bf4f534f03c31aaf4c297
Type_Guja <gusamogus84@yandex.ru>
2026-04-26 05:28:23 +0400

    rust

---
+++ alkash3d-execfile/target/debug/build/winapi-2f060f0394207c0d/build_script_build-2f060f0394207c0d.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-execfile\target\debug\build\winapi-2f060f0394207c0d\build_script_build-2f060f0394207c0d.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-execfile\target\debug\build\winapi-2f060f0394207c0d\build_script_build-2f060f0394207c0d.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs:
---
+++ alkash3d-rust/target/debug/deps/pin_project_lite-55c550245bfe95bc.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\debug\deps\pin_project_lite-55c550245bfe95bc.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\debug\deps\libpin_project_lite-55c550245bfe95bc.rmeta: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.17\src\lib.rs:
