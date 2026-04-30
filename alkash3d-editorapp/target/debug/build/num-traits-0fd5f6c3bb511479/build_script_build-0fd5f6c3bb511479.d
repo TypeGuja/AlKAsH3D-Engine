a0d52fd61dedcce6ce39630828b39a6706830b8f
Type_Guja <gusamogus84@yandex.ru>
2026-04-30 21:40:03 +0400

    rust

---
+++ alkash3d-editorapp/target/debug/build/num-traits-0fd5f6c3bb511479/build_script_build-0fd5f6c3bb511479.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-editorapp\target\debug\build\num-traits-0fd5f6c3bb511479\build_script_build-0fd5f6c3bb511479.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-editorapp\target\debug\build\num-traits-0fd5f6c3bb511479\build_script_build-0fd5f6c3bb511479.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\num-traits-0.2.19\build.rs:
---
+++ alkash3d-editorapp/target/debug/deps/scopeguard-9dad9f3674d4570a.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-editorapp\target\debug\deps\scopeguard-9dad9f3674d4570a.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\scopeguard-1.2.0\src\lib.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-editorapp\target\debug\deps\libscopeguard-9dad9f3674d4570a.rmeta: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\scopeguard-1.2.0\src\lib.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\scopeguard-1.2.0\src\lib.rs:
