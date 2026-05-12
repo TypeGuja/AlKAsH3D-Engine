e16cbcdb3363ac08dca8c0f82adbc7d81e1f2765
Type_Guja <gusamogus84@yandex.ru>
2026-05-12 19:28:03 +0400

    makefiles

---
+++ alkash3d-rust/target/debug/build/winit-9c1c8940b8276365/build_script_build-9c1c8940b8276365.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\debug\build\winit-9c1c8940b8276365\build_script_build-9c1c8940b8276365.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winit-0.31.0-beta.2\build.rs
+
+C:\Users\user\Documents\GitHub\AlKAsH3D-Engine\alkash3d-rust\target\debug\build\winit-9c1c8940b8276365\build_script_build-9c1c8940b8276365.exe: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winit-0.31.0-beta.2\build.rs
+
+C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winit-0.31.0-beta.2\build.rs:
